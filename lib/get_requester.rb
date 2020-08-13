84906134dc706d62594497ff320ebe65fa7ecb17
Eugene O'Neil <ejo7220@gmail.com>
2020-08-13 16:52:03 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,11 +2,11 @@ require 'net/http'
 require 'open-uri'
 require 'json'
  
-class GetRequester(url)
+class GetRequester
   
   URL = url
   
-  def get_response_body
+  def get_response_body(url)
     
   end
 end
